0fc8aca40d7319cbbf80bf10a91cb139ca21da96
mtownnymobilefingerprinting-commits <mtownnymobilefingerprinting@gmail.com>
2026-08-18 01:07:01 +0000

    Remove static export config to enable dynamic API routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export', <-- REMOVE OR COMMENT OUT THIS LINE
   images: {
     unoptimized: true,
   },
